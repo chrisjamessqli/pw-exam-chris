274db3c7e61e3d3e4a18facd8c0cc25206b6e700
Christopher James <cjames@sqli.com>
2025-07-07 19:45:05 +0200

    test:  extra check for login

--- tests/specs/pw-exam.spec.ts
+++ tests/specs/pw-exam.spec.ts
@@ -1,14 +1,26 @@
 import { test as base, expect } from '@playwright/test';
 import { test } from '../fixtures/fixure';
+import { request } from 'http';
 
-test('register new user success', async ({ page, pageManager, user }) => {
+test('register new user success', async ({ page, pageManager, user, request }) => {
 
   await page.goto('https://parabank.parasoft.com/');
   await expect(page).toHaveTitle(/ParaBank/);
   await pageManager.onLeftPanel().clickRegisterLink();
   await pageManager.onRegisterForm().fillRegisterForm(user);
-  await pageManager.onRegisterForm().submitRegisterForm();
+  await pageManager.onRegisterForm().submitRegisterForm()
   await expect (pageManager.onRegisterForm().registrationSuccessMessage).toBeVisible();
+
+  // Check via API that newly created user can login.
+  const loginReponse = await request.post('https://parabank.parasoft.com/parabank/login.htm', {
+    maxRedirects: 0,  // Ensure the API request doesn't get redirected resulting in 200 status code.
+    form: {
+      username: user.username,
+      password: user.password
+    }
+  });
+  expect(loginReponse.status()).toBe(302);
+
   await pageManager.onLeftPanel().clickLogoutLink();
   await expect (pageManager.onLeftPanel().registerLink).toBeVisible();
 
