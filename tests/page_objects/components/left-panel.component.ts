b1fcc98688eaff5d0a9cb9510266a20258a16895
Christopher James <cjames@sqli.com>
2025-07-08 12:52:55 +0200

    test: extra checks for FE login

--- tests/page_objects/components/left-panel.component.ts
+++ tests/page_objects/components/left-panel.component.ts
@@ -1,14 +1,21 @@
 import { Locator, Page } from "@playwright/test";
+import { User } from "../../model/faker-user";
 
 export class LeftPanelComponent {
     readonly page: Page;
     readonly registerLink: Locator;
     readonly logoutLink: Locator;
+    readonly usernameInput: Locator;
+    readonly passwordInput: Locator;
+    readonly loginButton: Locator;
 
     constructor(page: Page) {
         this.page = page;
         this.registerLink = page.locator("#loginPanel a[href*='register']");
         this.logoutLink = page.locator("a[href*='logout']");
+        this.usernameInput = page.locator("#loginPanel input[name='username']");
+        this.passwordInput = page.locator("#loginPanel input[name='password']");
+        this.loginButton = page.locator("#loginPanel input[type='submit']");
     }
 
     async clickRegisterLink() {
@@ -17,5 +24,14 @@ export class LeftPanelComponent {
 
     async clickLogoutLink() {
         await this.logoutLink.click();
+    }
+
+    async fillLoginForm(user: User) {
+        await this.usernameInput.fill(user.username);
+        await this.passwordInput.fill(user.password);
+    }
+
+    async submitLoginForm() {
+        await this.loginButton.click();
     }
 }
--- tests/specs/pw-exam.spec.ts
+++ tests/specs/pw-exam.spec.ts
@@ -5,14 +5,16 @@ test('register new user success', async ({ page, pageManager, user, request }) =
 
   await page.goto('https://parabank.parasoft.com/');
   await expect(page).toHaveTitle(/ParaBank/);
+  
+  // Register new user successfully via FE
   await pageManager.onLeftPanel().clickRegisterLink();
   await pageManager.onRegisterForm().fillRegisterForm(user);
   await pageManager.onRegisterForm().submitRegisterForm()
   await expect (pageManager.onRegisterForm().registrationSuccessMessage).toBeVisible();
 
-  // Check via API that newly created user can login.
+  // Check via API that newly created user can login
   const loginReponse = await request.post('https://parabank.parasoft.com/parabank/login.htm', {
-    maxRedirects: 0,  // Ensure the API request doesn't get redirected resulting in 200 status code.
+    maxRedirects: 0,  // Ensure the API request doesn't get redirected resulting in 200 status code
     form: {
       username: user.username,
       password: user.password
@@ -20,7 +22,13 @@ test('register new user success', async ({ page, pageManager, user, request }) =
   });
   expect(loginReponse.status()).toBe(302);
 
+  // Logout via FE
   await pageManager.onLeftPanel().clickLogoutLink();
   await expect (pageManager.onLeftPanel().registerLink).toBeVisible();
+
+  // Login via FE
+  await pageManager.onLeftPanel().fillLoginForm(user);
+  await pageManager.onLeftPanel().submitLoginForm();
+  await expect(pageManager.onLeftPanel().logoutLink).toBeVisible();
 
 });
